d546c1590dd77e5682269e014d71f8a174dc0bc7
Chris Reeves <chrisreeves@Chriss-MacBook-Air.local>
2013-07-19 12:50:10 +0100

    Added chris manifest file

---
+++ modules/people/manifests/chris.pp
@@ -0,0 +1,16 @@
+class people::chris {
+
+  # Development
+  include vim
+  include zsh
+  include tmux
+  include iterm2::dev
+
+  # Productivity
+  include chrome
+  include sparrow
+
+  # Entertainment
+  include spotify
+
+}
