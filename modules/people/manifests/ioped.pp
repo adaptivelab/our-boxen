4881c3be07ede7eeaebe4339212cdcf6c1e69a81
Pedro Maltez <pedro@adaptivelab.co.uk>
2013-07-25 14:33:42 +0100

    Add adium, alfred, and vlc to Puppetfile and updated personal manifest.

--- modules/people/manifests/ioped.pp
+++ modules/people/manifests/ioped.pp
@@ -4,20 +4,24 @@ class people::ioped {
   include zsh
   include iterm2::dev
   include sublime_text_2
-  sublime_text_2::package { 'Emmet':
-    source => 'sergeche/emmet-sublime'
-  }
+  # sublime_text_2::package { 'Emmet':
+  #   source => 'sergeche/emmet-sublime'
+  # }
 
   # Productivity
   include chrome
+  include firefox
+  include alfred
 
   # Utilities
-  # include vlc
-  # include adium
+  include vlc
+  include adium
+
+  # Entertainment
+  include spotify
 
   # Install Oh My Zsh
   repository { "/Users/${::luser}/.oh-my-zsh":
     source => "git://github.com/robbyrussell/oh-my-zsh.git",
   }
-
 }
