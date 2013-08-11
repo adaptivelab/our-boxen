2f9462980aa39aa95be29281ddbf01a81ff9cedf
Ben Jackson <ben@adaptivelab.com>
2013-08-11 19:34:46 +0100

    Ben - iterm prefs

--- modules/benjackson/manifests/iterm2-preferences.pp
+++ modules/benjackson/manifests/iterm2-preferences.pp
@@ -2,8 +2,10 @@ class benjackson::iterm2-preferences {
 
   file { "/Users/${::boxen_user}/Library/Preferences/com.googlecode.iterm2.plist":
     content => template("benjackson/com.googlecode.iterm2.plist.erb"),
-    owner => $::boxen_user,
-    group => "staff"
+    owner => root,
+    group => admin
   }
+
+
 
 }
