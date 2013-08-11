6113450dd9eec883196c474983296b2e39e49520
Ben Jackson <ben@adaptivelab.com>
2013-08-11 17:51:28 +0100

    Ben - added sublime settings

---
+++ modules/benjackson/manifests/sublime-preferences.pp
@@ -0,0 +1,10 @@
+class benjackson::sublime-preferences {
+
+  file { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+    content => template("${::boxen_user}/Preferences.sublime-settings.erb"),
+    group   => "staff",
+    owner   => $::boxen_user,
+    require => Class["sublime_text_2"]
+  }
+
+}
--- modules/people/manifests/benjackson.pp
+++ modules/people/manifests/benjackson.pp
@@ -3,9 +3,12 @@ class people::benjackson {
   include bash
   include bash::completion
   include iterm2::stable
+  include sublime_text_2
+  include font::source-code-pro
 
   include benjackson::osx-settings
   include benjackson::git-config
   include benjackson::dotfiles
+  include benjackson::sublime-preferences
 
 }
