52c294ff41d581dd6b6a10b75e57477db414cd92
Ben Jackson <ben@adaptivelab.com>
2013-08-11 19:50:01 +0100

    Ben - set iterm perfs file user/group correctly

--- modules/benjackson/manifests/iterm2-preferences.pp
+++ modules/benjackson/manifests/iterm2-preferences.pp
@@ -2,8 +2,8 @@ class benjackson::iterm2-preferences {
 
   file { "/Users/${::boxen_user}/Library/Preferences/com.googlecode.iterm2.plist":
     content => template("benjackson/com.googlecode.iterm2.plist.erb"),
-    owner => root,
-    group => admin
+    owner => $::boxen_user,
+    group => "staff"
   }
 
 
