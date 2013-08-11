5537f531e35df87be0d0957635de901409f1e612
Ben Jackson <ben@adaptivelab.com>
2013-08-11 17:53:02 +0100

    Ben - fixed sublime preferences

--- modules/benjackson/manifests/sublime-preferences.pp
+++ modules/benjackson/manifests/sublime-preferences.pp
@@ -1,7 +1,7 @@
 class benjackson::sublime-preferences {
 
   file { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
-    content => template("${::boxen_user}/Preferences.sublime-settings.erb"),
+    content => template("benjackson/Preferences.sublime-settings.erb"),
     group   => "staff",
     owner   => $::boxen_user,
     require => Class["sublime_text_2"]
