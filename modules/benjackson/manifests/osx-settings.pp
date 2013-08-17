acb61f64eed6ed7da02a2ccbfb35b447f9c82979
Ben Jackson <ben@adaptivelab.com>
2013-08-17 15:38:18 +0100

    Ben - updated osx settings

--- modules/benjackson/manifests/osx-settings.pp
+++ modules/benjackson/manifests/osx-settings.pp
@@ -1,15 +1,33 @@
 class benjackson::osx-settings {
 
-  file { "${::boxen_srcdir}/osx-settings.sh":
-    content => template("benjackson/osx-settings.sh.erb"),
+  if $::boxen_user == "ben" {
+    $type = "home"
+  } else {
+    $type = "work"
+  }
+
+  file { "${::boxen_srcdir}/root-osx-settings.sh":
+    content => template("benjackson/root-osx-settings.sh.erb"),
+    group   => "staff"
+  }
+
+  file { "${::boxen_srcdir}/user-osx-settings.sh":
+    content => template("benjackson/user-osx-settings.sh.erb"),
     group   => "staff",
     owner   => $::boxen_user
   }
 
-  exec { "sh ${::boxen_srcdir}/osx-settings.sh":
+  exec { "${boxen::config::homebrewdir}/bin/bash -v ${::boxen_srcdir}/root-osx-settings.sh":
+    user        => "root",
+    path        => ["/bin", "/usr/bin", "/usr/sbin", "/usr/local/bin"],
+    subscribe   => File["${::boxen_srcdir}/root-osx-settings.sh"],
+    refreshonly => true
+  }
+
+  exec { "${boxen::config::homebrewdir}/bin/bash -v ${::boxen_srcdir}/user-osx-settings.sh":
     user        => $::boxen_user,
-    path        => ["/bin", "/usr/bin", "/usr/local/bin"],
-    subscribe   => File["${::boxen_srcdir}/osx-settings.sh"],
+    path        => ["/bin", "/usr/bin", "/usr/sbin", "/usr/local/bin"],
+    subscribe   => File["${::boxen_srcdir}/user-osx-settings.sh"],
     refreshonly => true
   }
 
