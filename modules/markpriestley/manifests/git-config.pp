f13b0c52c56a8a60bd369591df45e8a5857391cb
Mark Priestley <mark@adaptivelab.com>
2013-08-27 23:12:12 +0100

    Added initial personal manifest and modules

---
+++ modules/markpriestley/manifests/git-config.pp
@@ -0,0 +1,10 @@
+class markpriestley::git-config {
+
+  git::config::global {
+    "user.name":
+      value => "Mark Priestley";
+    "user.email":
+      value => "mark@adaptivelab.com";
+  }
+
+}
