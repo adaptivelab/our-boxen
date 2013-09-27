548ef5b080dab2670411504c57e9dcd50484f93f
Kimberley Szelong <kim@adaptivelab.co.uk>
2013-09-27 11:33:02 +0100

    Added initial personal manifest and modules

--- modules/people/manifests/kymsze.pp
+++ modules/people/manifests/kymsze.pp
@@ -3,15 +3,26 @@ class people::kymsze {
 	#Development
   include sublime_text_2
   include zsh
+  include vim
+  include ruby 
+  include phantomjs::1_9_0
+  include postgresql
+  include foreman
+  include redis
+  include mongodb
 
   #Browsers
   include firefox
   include chrome
-
+  include alfred
 
   #Productivity
   include caffeine
+  #include wunderlist
+  #include googledrive
 
+  # Utilities
+  include vlc
 
   # Oh My Zsh
   repository { "/Users/${::luser}/.oh-my-zsh":
@@ -26,4 +37,15 @@ class people::kymsze {
     require => Repository["/Users/${::luser}/.oh-my-zsh"],
   }
 
+  class { 'ruby::global':
+    version => '1.9.3-p448'
+  }
+
+  ruby::gem { 'bundler for 1.9.3-p448':
+    gem     => 'bundler',
+    ruby    => '1.9.3-p448',
+    version => '~> 1.3.5'
+  }
+
 }
+
