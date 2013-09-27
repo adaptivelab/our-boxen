bc986e0eb105a2052c8c1acf85e9934d2f5de31f
Kimberley Szelong <kim@adaptivelab.co.uk>
2013-09-27 10:20:38 +0100

    updated kymsze boxen mainfest

--- modules/people/manifests/kymsze.pp
+++ modules/people/manifests/kymsze.pp
@@ -1,5 +1,29 @@
 class people::kymsze {
-
+	
+	#Development
   include sublime_text_2
+  include zsh
+
+  #Browsers
+  include firefox
+  include chrome
+
+
+  #Productivity
+  include caffeine
+
+
+  # Oh My Zsh
+  repository { "/Users/${::luser}/.oh-my-zsh":
+    source => "git://github.com/robbyrussell/oh-my-zsh.git"
+  }
+
+  # Oh My Zsh Theme
+  file { "/Users/${::luser}/.oh-my-zsh/themes/ks_theme.zsh-theme":
+    ensure => link,
+    mode => '0644',
+    target => "/Users/${::luser}/.oh-my-zsh/themes/ks_theme.zsh-theme",
+    require => Repository["/Users/${::luser}/.oh-my-zsh"],
+  }
 
 }
