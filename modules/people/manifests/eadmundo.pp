d7e5c14d9c81cc704f731ea26927f78015be2b0b
Edward Stone <edwardjstone@yahoo.com>
2013-10-04 12:05:23 +0100

    osx preferences

--- modules/people/manifests/eadmundo.pp
+++ modules/people/manifests/eadmundo.pp
@@ -17,5 +17,15 @@ class people::eadmundo {
       'openconnect'
      ]:
    }
+   
+  include osx::finder::unhide_library
+  include osx::dock::clear_dock
+  include osx::dock::hide_indicator_lights
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::disable_key_press_and_hold
+  include osx::disable_app_quarantine
+  include osx::no_network_dsstores
 
 }
